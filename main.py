70094edcf73bbf448fe3663f8b42e9214b7944b1
yeoshuheng <100367948+yeoshuheng@users.noreply.github.com>
2024-05-13 17:19:49 +0800

    chore: Add output files

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from src import MatchingEngine
 
 if __name__ == "__main__":
-    engine = MatchingEngine("./C2C-2024-DataSet/DataSets/example-set/input_clients.csv",
-                            "./C2C-2024-DataSet/DataSets/example-set/input_instruments.csv",
-                            "./C2C-2024-DataSet/DataSets/example-set/input_orders.csv")
+    engine = MatchingEngine("./C2C-2024-DataSet/DataSets/test-set/input_clients.csv",
+                            "./C2C-2024-DataSet/DataSets/test-set/input_instruments.csv",
+                            "./C2C-2024-DataSet/DataSets/test-set/input_orders.csv")
     engine.run()
