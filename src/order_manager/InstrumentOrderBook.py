1129f8b02d6d5b6f1670f8d32f9741eb3d17c1d6
prawnzyy <jamcookies77@gmail.com>
2024-05-13 13:31:25 +0800

    Add InOrder

--- src/order_manager/InstrumentOrderBook.py
+++ src/order_manager/InstrumentOrderBook.py
@@ -1,8 +1,19 @@
-import buyHeap, sellHeap
+import buyHeap, sellHeap, report_manager
 
 class InstrumentOrderBook:
-    def __init__(self, id):
-        self.id_
+    def __init__(self, id, reportManager):
+        self.id = id
         self.buyHeap = buyHeap()
         self.sellHeap = sellHeap()
+        self.market = [] * 10
+        self.reportManager
+
+    def getSellHeap(self):
+        return self.sellHeap
+    
+    def getBuyHeap(self):
+        return self.buyHeap
+    
+    def getMarket(self):
+        return self.market
         
